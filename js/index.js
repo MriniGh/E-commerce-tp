6e612dd241a8e4b42f9859394a6b5152b5904833
aymenft <tifakaymen@gmail.com>
2023-11-23 11:59:29 +0100

    Feat(add to cart): add to cart function using locale storage, another function to fetch cart items in page cart.html

--- js/index.js
+++ js/index.js
@@ -11,3 +11,31 @@
       }
     });
   });
+
+
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var number = document.getElementById('number');
+  
+    if (number.innerHTML.trim() === "") {
+      number.style.display = 'none';
+    }
+  });
+
+
+
+  function addToCart(buttonElement) {
+    var boxElement = buttonElement.closest('.box-div');
+
+    var productName = boxElement.querySelector('.detail-box h6:first-child').innerText.trim();
+    var price = boxElement.querySelector('.detail-box h6:last-child span').innerText.trim().substring(1);
+    var ImgSrc = boxElement.querySelector('.img-box img').getAttribute('src') ;
+  
+
+    var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+    cartItems.push({ productName: productName, price: price ,img :ImgSrc});
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    alert('Product added to cart!');
+}
