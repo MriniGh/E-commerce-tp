47e34c9a2de8ead27f06d491e9263bcd960c5de7
aymenft <tifakaymen@gmail.com>
2023-11-23 15:31:38 +0100

    Feat(fetch api data):fetch products data and categories , update filter function

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
     var boxes = document.querySelectorAll('.box-div');
 
     boxes.forEach(function (box) {
-      var boxCategory = box.getAttribute('data-category');
+      var boxCategory = box.querySelector('.productCategory').value;
       if (selectedCategory === 'Choose...' || selectedCategory === boxCategory) {
         box.style.display = 'block';
       } else {
@@ -39,3 +39,98 @@
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
     alert('Product added to cart!');
 }
+
+
+
+function renderProducts(products) {
+  var productSection = document.getElementById('ProductSection');
+
+  for (product of products) {
+
+    var productCard = document.createElement('div');
+    productCard.className = 'col-sm-6 col-md-4 col-lg-3 box-div';
+    productCard.innerHTML = `
+      <div class="box">
+        <div class="img-box">
+          <img src="${product.images[0]}" alt="${product.title}">
+        </div>
+        <div class="detail-box">
+          <h6>${product.title}</h6>
+          <h6>Price <span>${product.price}</span></h6>
+        </div>
+        <div class="new">
+          <span>New</span>
+        </div>
+        <div class="add-to-cart">
+          <button type="button" class="btn btn-success" onclick="addToCart(this)">
+            Add to Cart
+          </button>
+        </div>
+
+        <input type="hidden" class="productCategory" value="${product.category}">
+
+      </div>
+    `;
+
+    productSection.appendChild(productCard);
+  };
+}
+
+
+function rendercategories(categories) {
+  var SelectList = document.getElementById('select');
+
+
+  for (categorie of categories) {
+    SelectList.innerHTML += `
+    <option value="${categorie}">${categorie}</option>
+    `;
+  };
+}
+
+
+
+
+
+// fetch products from api
+document.addEventListener('DOMContentLoaded', function () {
+      fetch('https://dummyjson.com/products' ,{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response=>{
+        if(!response.ok){
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data =>{
+        renderProducts(data.products);
+      })
+      .catch(error =>console.error('Fetch error:',error));
+});
+
+
+//fetch categories from api
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://dummyjson.com/products/categories' ,{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response=>{
+    if(!response.ok){
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data =>{
+    rendercategories(data)
+  })
+  .catch(error =>console.error('Fetch error:',error));
+});
+
+
